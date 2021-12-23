be64439438f0c61742abf6955b55d6c1f2fdf646
Wolfgang Förster <wolfoerster@gmx.de>
2021-12-23 17:12:02 +0100

    reformat documents

--- WFTools3DDemo/App.xaml.cs
+++ WFTools3DDemo/App.xaml.cs
@@ -1,14 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Windows;
+﻿//******************************************************************************************
+// Copyright © 2021 Wolfgang Foerster (wolfoerster@gmx.de)
+//
+// This file is part of the WFTools3D project which can be found on github.com
+//
+// WFTools3D is free software: you can redistribute it and/or modify it under the terms 
+// of the GNU General Public License as published by the Free Software Foundation, 
+// either version 3 of the License, or (at your option) any later version.
+// 
+// WFTools3D is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************************
 
 namespace WFTools3DDemo
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
-	public partial class App : Application
-	{
-	}
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
 }
