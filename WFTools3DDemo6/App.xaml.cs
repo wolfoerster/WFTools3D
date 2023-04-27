e5c3c1f2ac11f5276702018c763f77db2c6dbfb0
Wolfgang Foerster <wolfgang_foerster@trimble.com>
2023-04-27 16:38:36 +0200

    still no success

---
+++ WFTools3DDemo6/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿//******************************************************************************************
+// Copyright © 2023 Wolfgang Foerster (wolfoerster@gmx.de)
+//
+// This file is part of the WFTools3D project which can be found on github.com.
+//
+// WFTools3D is free software: you can redistribute it and/or modify it under the terms 
+// of the GNU General Public License as published by the Free Software Foundation, 
+// either version 3 of the License, or (at your option) any later version.
+// 
+// WFTools3D is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************************
+
+namespace WFTools3DDemo6;
+
+using System;
+using System.Windows;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        var theme = "PresentationFramework.Aero;V3.0.0.0;31bf3856ad364e35;component\\themes/aero.normalcolor.xaml";
+        var uri = new Uri(theme, UriKind.Relative);
+        var resources = Application.LoadComponent(uri) as ResourceDictionary;
+        this.Resources.MergedDictionaries.Add(resources);
+    }
+}
---
+++ WFTools3DDemo6/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿//******************************************************************************************
+// Copyright © 2023 Wolfgang Foerster (wolfoerster@gmx.de)
+//
+// This file is part of the WFTools3D project which can be found on github.com.
+//
+// WFTools3D is free software: you can redistribute it and/or modify it under the terms 
+// of the GNU General Public License as published by the Free Software Foundation, 
+// either version 3 of the License, or (at your option) any later version.
+// 
+// WFTools3D is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************************
+
+namespace WFTools3DDemo6;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+        var tb = new TextBox();
+    }
+
+    protected override void OnKeyDown(KeyEventArgs e)
+    {
+        base.OnKeyDown(e);
+        if (e.Key == Key.Escape)
+            Close();
+    }
+}
